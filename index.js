b046a36203d9ab980f3ed6598bdd44973f0475e7
Eric Nguyen <enguyen89141@gmail.com>
2019-07-25 16:30:49 -0700

    resolved all prior issues, loaded issues page, added comments

--- index.js
+++ index.js
@@ -1,40 +1,39 @@
 'use strict'
 
-const medicKey = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImVuZ3V5ZW44OTE0MUBnbWFpbC5jb20iLCJyb2xlIjoiVXNlciIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL3NpZCI6IjU0NjkiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3ZlcnNpb24iOiIyMDAiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL2xpbWl0IjoiOTk5OTk5OTk5IiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9tZW1iZXJzaGlwIjoiUHJlbWl1bSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbGFuZ3VhZ2UiOiJlbi1nYiIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvZXhwaXJhdGlvbiI6IjIwOTktMTItMzEiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL21lbWJlcnNoaXBzdGFydCI6IjIwMTktMDctMjQiLCJpc3MiOiJodHRwczovL3NhbmRib3gtYXV0aHNlcnZpY2UucHJpYWlkLmNoIiwiYXVkIjoiaHR0cHM6Ly9oZWFsdGhzZXJ2aWNlLnByaWFpZC5jaCIsImV4cCI6MTU2NDAzMjcyNiwibmJmIjoxNTY0MDI1NTI2fQ.vRcjL1Qh25cJqyvMnWFD31gyzXAHW4DjbgJ7BpHOj7I';
-const medicURL = 'https://sandbox-healthservice.priaid.ch/'
-let zipCode = ''
-let userLat = ''
-let userLong = ''
-let symptoms = ''
-let symptomsArr = []
+let medicKey = ''; //stores ApiMedic API key
+const medicURL = 'https://sandbox-healthservice.priaid.ch/'; //URL base to fetch data
+let zipCode = ''; //user's zip code
+let userLat = ''; //user's latitude
+let userLong = ''; //user's longitude
+let issues = ''; //responseJson Object of issues from ApiMedic
+let symptoms = ''; //responseJson Object of symptoms from ApiMedic
+let symptomsArr = []; //array of user's symptoms that were checked
+const api_key = 'enguyen89141@gmail.com' 
+var uri = 'https://sandbox-authservice.priaid.ch/login'; //login url to fetch token 
+var secret_key = "n6X3ByDw97Eod8W2Q"; //ApiMedic pass to get token 
+var computedHash = CryptoJS.HmacMD5(uri,secret_key); 
+var computedHashString = computedHash.toString(CryptoJS.enc.Base64);
 
-function fetchSymptoms() {
-    var uri = 'https://sandbox-authservice.priaid.ch/login';
-    var secret_key = "n6X3ByDw97Eod8W2Q";
-    var computedHash = CryptoJS.HmacMD5(uri,secret_key);
-    var computedHashString = computedHash.toString(CryptoJS.enc.Base64);
-    console.log(computedHashString)
-    fetch(medicURL + 'symptoms?token=' + computedHashString + '&format=json&language=en-gb')
-    .then(response => response.json())
-    .then(responseJson =>
-         populateSymptoms(responseJson))
-    .catch(error => alert('Something went wrong. Please try again later.'))
-}
 
-function populateSymptoms(responseJson) {
-    symptoms = responseJson
-    for (let i = 0; i < 21; i++){
-        $('#symptomsColumn1').append(`<input type="checkbox" class="checkbox" id="${symptoms[i].ID}" name="${symptoms[i].Name}"><label for="${symptoms[i].ID}">${symptoms[i].Name}</label><br>`)
+function showForm() { //1 retrieves token from ApiMedic, hides welcome text, and shows general information collection screen
+    $('#start').on('click', function(event) {
+        event.preventDefault();
+        fetch('https://sandbox-authservice.priaid.ch/login',{
+            method: 'POST',
+            headers: {
+                'Authorization': 'Bearer ' + api_key + ':' + computedHashString
+            }})
+            .then(response => response.json())
+            .then(function(responseJson) {
+                medicKey = responseJson.Token;
+            })
+        $('.welcomeText').css('display', 'none')
+        $('.container').css('border', 'none')
+        $('.genInfo').css('display', 'block')
+            })
     }
-    for (let j = 20; j < 41; j++){
-        $('#symptomsColumn2').append(`<input type="checkbox" class="checkbox" id="${symptoms[j].ID}" name="${symptoms[j].Name}"><label for="${symptoms[j].ID}">${symptoms[j].Name}</label><br>`)
-    }
-    for (let k = 41; k < 62; k++){
-        $('#symptomsColumn3').append(`<input type="checkbox" class="checkbox" id="${symptoms[k].ID}" name="${symptoms[k].Name}"><label for="${symptoms[k].ID}">${symptoms[k].Name}</label><br>`)
-    }
-}
-
-function getLocation() {
+ 
+function getLocation() { //2.1 gets user's longitude and latitude coordinates when they click on get my current location 
     $('.currentLocation').on('click', function(event) {
         if ("geolocation" in navigator) {
             navigator.geolocation.getCurrentPosition(function(position) {
@@ -47,22 +46,37 @@ function getLocation() {
             alert('Sorry, we are unable to determine you location at this time. Please enter in a zip code.')
         }
     })
+}   
+
+function fetchSymptoms() { //2.2 Uses token showForm to fetch symptoms
+
+    fetch(medicURL + 'symptoms?token=' + medicKey + '&format=json&language=en-gb')
+    .then(response => response.json())
+    .then(responseJson =>
+         populateSymptoms(responseJson))
+    .catch(error => alert('Something went wrong. Please try again later.'))
 }
 
-function showForm() {
-    $('#start').on('click', function(event) {
-        event.preventDefault();
-        $('.welcomeText').css('display', 'none')
-        $('.container').css('border', 'none')
-        $('.genInfo').css('display', 'block')
-    })
+
+function populateSymptoms(responseJson) { //2.3 populates symptoms into 3 columns
+    symptoms = responseJson;
+    for (let i = 0; i < 21; i++){
+        $('#symptomsColumn1').append(`<input type="checkbox" class="checkbox" id="${symptoms[i].ID}" value="${symptoms[i].ID}" name="${symptoms[i].Name}"><label for="${symptoms[i].ID}">${symptoms[i].Name}</label><br>`)
+    }
+    for (let j = 20; j < 41; j++){
+        $('#symptomsColumn2').append(`<input type="checkbox" class="checkbox" id="${symptoms[j].ID}" value="${symptoms[j].ID}" name="${symptoms[j].Name}"><label for="${symptoms[j].ID}">${symptoms[j].Name}</label><br>`)
+    }
+    for (let k = 41; k < 62; k++){
+        $('#symptomsColumn3').append(`<input type="checkbox" class="checkbox" id="${symptoms[k].ID}" value="${symptoms[k].ID}" name="${symptoms[k].Name}"><label for="${symptoms[k].ID}">${symptoms[k].Name}</label><br>`)
+    }
 }
-function showSymptoms() {
+
+
+function showSymptoms() { //2.4 ensures user inputs zip code or coordinates, hides general infomation page and populates/shows symptoms page
     $('.genInfo').submit(function(event) {
         event.preventDefault();
-        let zipCode = $('.zip').val();
-        /* ask David why the OR in this if function isn't working properly and about symptoms clear*/
-        if($('.zip').val() === '' || $('.latitude').val() === '' ) {
+        let zipCode = $('.zip').val();        
+        if($('.zip').val() === '' && $('.latitude').val() === '' ) {
                 alert("Please enter a zip code or select my current location.")
             } else {
                 $('form').css('display', 'none')
@@ -74,19 +88,41 @@ function showSymptoms() {
     })
 
 }
-/*ask about getting id value from checkbox
-function showIssues() {
+
+
+function fetchIssues() { //3.1 fetches and populates issues page 
+    fetch(medicURL + 'issues?token=' + medicKey + '&format=json&language=en-gb')
+    .then(response => response.json())
+    .then(responseJson =>
+         populateIssues(responseJson))
+    .catch(error => alert('Something went wrong. Please try again later.'))
+}
+
+function populateIssues(responseJson) {//same as above
+    issues = responseJson;
+    for (let i = 0; i < 25; i++){
+        $('#issuesColumn1').append(`<input type="checkbox" class="checkbox" id="${issues[i].ID}" value="${issues[i].ID}" name="${issues[i].Name}"><label for="${issues[i].ID}">${issues[i].Name}</label><br>`)
+    }
+    for (let j = 25; j < 50; j++){
+        $('#issuesColumn2').append(`<input type="checkbox" class="checkbox" id="${issues[j].ID}" value="${issues[j].ID}" name="${issues[j].Name}"><label for="${issues[j].ID}">${issues[j].Name}</label><br>`)
+    }
+}
+
+function showIssues() { //3.2 Stores ID numbers from symptoms page into an array, hides the page, and shows/populates issues page 
     $('.symptomsPage').submit(function(event) {
         event.preventDefault();
         $('.symptomsPage input[type="checkbox"]:checked').each(function() {
-            console.log($(this).ID)
+                symptomsArr.push($(this).val());
+        });
+        $('.symptomsPage').css('display', 'none')
+        fetchIssues();
+        $('.issuesPage').css('display', 'flex')
+        $('.issuesPage').css('flex-wrap', 'wrap')
+        $('.issuesPage').css('justify-content', 'center')
         })
-        console.log(symptomsArr)
-    })
-}
+    }
 
-showIssues();
-*/
-showSymptoms();
 showForm();
+showIssues();
+showSymptoms();
 getLocation();
